2bffd2b4a06e5b219d33ecb6391e7b4c03915a62
m-kanno <akapanna@gmail.com>
2014-03-07 06:13:35 +0900

    get avator

--- index.js
+++ index.js
@@ -1,91 +1,91 @@
 'use strict';
 
-	var path = require('path');
-	var util = require('util');
-	var localStorage = localStorage || {};
+var path = require('path');
+var util = require('util');
+var localStorage = localStorage || {};
 
-	function make_path(added) {
-		return path.join('http://localhost:9999/', added);
+function make_path(added) {
+	return path.join('http://localhost:9999/', added);
+}
+
+var sessionId = {
+	set: function(id) {
+		localStorage.sessionId = id;
+	},
+
+	get: function() {
+		return localStorage.sessionId || $.cookie('atmosphere-session-id') || '';
 	}
+};
 
-	var sessionId = {
-		set: function(id) {
-			localStorage.sessionId = id;
-		},
+var http = {
+	request: function(apiPath, method, data) {
+		return $.ajax({
+							url: apiPath,
+							type: method,
+							dataType: 'text',
+							data: data,
+							headers: {
+								'atmosphere-session-id': sessionId.get()
+							},
+							cache: false
+						})
+						.done(function(res, _, xhr) {
+							sessionId.set(xhr.getResponseHeader('atmosphere-session-id'));
+						})
+						.fail(function(_, __, error) {
+							console.log(error);
+						});
+	},
 
-		get: function() {
-			return localStorage.sessionId || $.cookie('atmosphere-session-id') || '';
-		}
-	};
+	get: function(apiPath, data) {
+		return this.request(apiPath, 'GET', data);
+	},
 
-	var http = {
-		request: function(apiPath, method, data) {
-			return $.ajax({
-								url: apiPath,
-								type: method,
-								dataType: 'text',
-								data: data,
-								headers: {
-									'atmosphere-session-id': sessionId.get()
-								},
-								cache: false
-							})
-							.done(function(res, _, xhr) {
-								sessionId.set(xhr.getResponseHeader('atmosphere-session-id'));
-							})
-							.fail(function(_, __, error) {
-								console.log(error);
-							});
-		},
-
-		get: function(apiPath, data) {
-			return this.request(apiPath, 'GET', data);
-		},
-
-		post: function(apiPath, data) {
-			return this.request(apiPath, 'POST', data);
-		}
-	};
+	post: function(apiPath, data) {
+		return this.request(apiPath, 'POST', data);
+	}
+};
 
 
-	(function bind_login() {
-		$('#login').on('click', function() {
-			http.post(
-				make_path('auth/login'),
-				util.format('{"user_id": "%s", "password": "%s"}', $('#login-id').val(), $('#login-password').val())
-			)
-			.done(function(res) {
-				if (JSON.parse(res).session_id) {
-					$('#entrance').css('display', 'none');
-					$('#content').show();
-				}
-			});
+(function bind_login() {
+	$('#login').on('click', function() {
+		http.post(
+			make_path('auth/login'),
+			util.format('{"user_id": "%s", "password": "%s"}', $('#login-id').val(), $('#login-password').val())
+		)
+		.done(function(res) {
+			if (JSON.parse(res).session_id) {
+				$('#entrance').css('display', 'none');
+				$('#content').show();
+			}
 		});
-	})();
+	});
+})();
 
-	(function bind_search() {
-		$('#search').on('click', function() {
-			http.get(
-				make_path('/messages/search'),
-				util.format('keywords=%s', $('#search-text').val())
-			)
-			.done(function(res) {
-				console.log(JSON.parse(res));
-			});
+(function bind_search() {
+	$('#search').on('click', function() {
+		http.get(
+			make_path('/messages/search'),
+			util.format('keywords=%s', $('#search-text').val())
+		)
+		.done(function(res) {
+			console.log(JSON.parse(res));
 		});
-	})();
+	});
+})();
 
-	(function bind_send() {
-		$('#send').on('click', function() {
-			http.post(
-				make_path('/messages/send'),
-				util.format('{"message": "%s", "reply_to": ""}', $('#send-text').val())
-			)
-			.done(function(res) {
-				console.log(JSON.parse(res));
-			});
+(function bind_send() {
+	$('#send').on('click', function() {
+		http.post(
+			make_path('/messages/send'),
+			util.format('{"message": "%s", "reply_to": ""}', $('#send-text').val())
+		)
+		.done(function(res) {
+			console.log(JSON.parse(res));
 		});
-	})();
+	});
+})();
 
 function timelineController($scope) {
 	// TODO タイミング
@@ -98,4 +98,8 @@ function timelineController($scope) {
 			});
 		});
 	});
+
+	$scope.avatarUrl = function(message) {
+		return make_path('user/avator') + util.format('?user_id=%s', message.created_by);
+	};
 }
